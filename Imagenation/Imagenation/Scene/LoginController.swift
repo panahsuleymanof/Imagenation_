f03a9f08ee1488a6344ef02e860597883b85e015
panahsuleymanof <panahsuleymanof@gmail.com>
2024-07-31 16:14:10 +0400

    tab bar and login controller

---
+++ Imagenation/Imagenation/Scene/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  Imagenation
+//
+//  Created by Panah Suleymanli on 31.07.24.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Imagenation/Imagenation/Scene/RegisterController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterController.swift
+//  Imagenation
+//
+//  Created by Panah Suleymanli on 31.07.24.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
