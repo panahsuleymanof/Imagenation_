c3bd4876c69ee86c7d5677426fe73439289bb6bb
panahsuleymanof <panahsuleymanof@gmail.com>
2024-08-09 16:59:59 +0400

    login design done

--- Imagenation/Imagenation/Scene/LoginController.swift
+++ Imagenation/Imagenation/Scene/LoginController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var emailFieldView: UIView!
+    @IBOutlet weak var pswdField: UITextField!
+    @IBOutlet weak var pswdFieldView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        titleDesing()
+        textFieldDesign()
     }
-
+    func titleDesing() {
+        self.title = "Login"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    func textFieldDesign() {
+        emailField.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        pswdField.attributedPlaceholder = NSAttributedString(
+            string: "Password",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+    }
 }
+
